b5318196c3aa9628e0fce7f41448b1e1cdaf6538
Le Hong ngan <86295743+hongngan45@users.noreply.github.com>
2022-01-18 12:06:44 +0700

    Update photos.php

--- 43_1951060885_LeThiHongNgan/fb_files/fb_view_profile/photos.php
+++ 43_1951060885_LeThiHongNgan/fb_files/fb_view_profile/photos.php
@@ -12,21 +12,26 @@
 <body bgcolor="#E9EAED">
 
 
-<div style="position:absolute;left:30%; display:none;  top:51%; height:9.8%; width:6.9%; background-color:#F6F7F8; z-index:1;" id="timeline_txt_background"> </div>
-<div style="position:absolute;left:31.5%; top:54%; font-weight:bold; z-index:1;"> <a href="view_profile.php?id=<?php echo $v_user_id; ?>" style="text-decoration:none; color:#3B59B0;" onMouseOver="on_timeline_txt();" onMouseOut="out_timeline_txt();">  Timeline </a> </div>
-<div style="position:absolute;left:37.1%; display:none; top:51%; height:9.8%; width:5.9%; background-color:#F6F7F8; z-index:1;" id="about_txt_background"> </div>
-<div style="position:absolute;left:38.3%; top:54%; font-weight:bold; z-index:1;"> <a href="about.php?id=<?php echo $v_user_id; ?>" style="text-decoration:none; color:#3B59B0;" onMouseOver="on_about_txt();" onMouseOut="out_about_txt();"> About </a>  </div>
-<div style="position:absolute;left:44.7%; top:54%; font-weight:bold; z-index:1; color:#000000;"> Photos  <samp style="color:#717171;"> <?php echo $photos_count; ?> </samp> </div>
+
+<div style="position:absolute;left:17.2%; top:6.5%; font-weight:bold; z-index:1;color: #b0b3b8;"> <a href="view_profile.php?id=<?php echo $v_user_id; ?>" style="text-decoration:none; color:#b0b3b8;" onMouseOver="on_timeline_txt();" onMouseOver="on_timeline_txt();" onMouseOut="out_timeline_txt();"> Bài viết </a> </div>
+
+<div style="position:absolute;left:23%; top:6.5%; font-weight:bold; z-index:1;"> <a href="about.php?id=<?php echo $v_user_id; ?>" style="text-decoration:none; color:#b0b3b8;" onMouseOver="on_about_txt();" onMouseOut="out_about_txt();"> About </a>  </div>
+<div style="position:absolute;left:34%; top:6.5%; font-weight:bold; z-index:1; color:#b0b3b8;"> Photos  <samp style="color:#717171;"> <?php echo $photos_count; ?> </samp> </div>
+<div style="position:absolute;left:40%; top:6.5%; font-weight:bold; z-index:1;"> <a  style="text-decoration:none; color:#b0b3b8;"> Xem thêm </a>  </div>
+<div style="position:absolute;left:53%; top:6.5%; font-weight:bold; z-index:1;"> <a  style="text-decoration:none; color:#b0b3b8;"> Thêm Vào tin </a>  </div>
+<div style="position:absolute;left:61%; top:6.5%; font-weight:bold; z-index:1;"> <a  style="text-decoration:none; color:#b0b3b8;"> Chỉnh sửa trang cá nhân  </a>  </div>
+<div style="position:absolute;left:75%; top:6.5%; font-weight:bold; z-index:1;"> <a  style="text-decoration:none; color:#b0b3b8;"> <i class="bi bi-list"></i> </a>  </div>
 
 
 
 
-<div style="position:absolute;left:15%;top:68%;height:12%;width:70%; background-color:#F6F7F8; box-shadow:0px -1px 5px 1px rgb(0,0,0);"> </div>
 
-<div style="position:absolute;left:16%;top:69%;"> <img src="img/photos.PNG"> </div>
+<div style="position:absolute;left:15%;top:7.3%;height:12%;width:70%; background-color:#F6F7F8; box-shadow:0px -1px 5px 1px rgb(0,0,0);"> </div>
+
+<div style="position:absolute;left:16%;top:7.5%;"> <img src="img/photos.PNG"> </div>
 <div style="position:absolute;left:19%;top:66.3%;"> <h2> Photos </h2> </div>
 
-<div style="position:absolute;left:15%; top:80%; height:125%; width:70%; background:#FFF; box-shadow:0px -1px 5px 1px rgb(0,0,0); z-index:-1;">
+<div style="position:absolute;left:15%; top:7.7%; height:125%; width:70%; background:#FFF; box-shadow:0px -1px 5px 1px rgb(0,0,0); z-index:-1;">
 </div>
 
 <script>
@@ -65,15 +70,15 @@ function close_profile_photo()
 </script>
 
 <div id="albums">
-<div style="position:absolute; left:18%; top:85%;">
+<div style="position:absolute; left:18%; top:8.5%;">
 <img src="../../fb_users/<?php echo $v_gender; ?>/<?php echo $v_email; ?>/Profile/<?php echo $profile_img; ?>" style="height:200; width:200; box-shadow:0px 0px 5px 1px rgb(0,0,0);" onClick="open_profile_photo_album()">
 </div>
-<div style="position:absolute; left:23%; top:118%;"> <h3> Profile </h3> </div>
+<div style="position:absolute; left:23%; top:8%;"> <h3> Anh đại  </h3> </div>
 
-<div style="position:absolute; left:41%; top:85%;">
+<div style="position:absolute; left:41%; top:8.5%;">
 <img src="../../fb_users/<?php echo $v_gender; ?>/<?php echo $v_email; ?>/Cover/<?php echo $cover_img; ?>" style="height:200; width:200; box-shadow:0px 0px 5px 1px rgb(0,0,0);" id="cover_photo" onClick="open_cover_photo_album()" >
 </div>
-<div style="position:absolute; left:46.4%; top:118%;"> <h3> Cover </h3> </div>
+<div style="position:absolute; left:46.4%; top:8%;"> <h3> Ảnh bìa </h3> </div>
 
 <?php
 	$img_array = array();
@@ -83,31 +88,31 @@ function close_profile_photo()
 	}
 ?>
 
-<div style="position:absolute; left:65%; top:85%;">
+<div style="position:absolute; left:65%; top:8.5%;">
 <img src="../../fb_users/<?php echo $v_gender; ?>/<?php echo $v_email; ?>/Post/<?php echo $img_array[0] ?>" style="height:200; width:200; box-shadow:0px 0px 5px 1px rgb(0,0,0);" id="timeline_photos" onClick="open_timeline_photos_album()">
 </div>
-<div style="position:absolute; left:70%; top:118%;"> <h3> Timeline </h3> </div>
+<div style="position:absolute; left:70%; top:8%;"> <h3> Anh tải lên  </h3> </div>
 </div>
 
 <!--profile_photo_album-->
 <div style="display:none;" id="profile_photo_album">
-<div style="position:absolute; left:21%; top:100%;">
+<div style="position:absolute; left:21%; top:8%;">
 <img src="../../fb_users/<?php echo $v_gender; ?>/<?php echo $v_email; ?>/Profile/<?php echo $profile_img; ?>" style="height:200; width:200; box-shadow:0px 0px 5px 1px rgb(0,0,0);" onClick="open_profile_photo()">
 </div>
-<div style="position:absolute; left:18%; top:85%;"> <img src="img/Go back.png" height="50" width="50" onClick="back()"> </div>
+<div style="position:absolute; left:18%; top:8%;"> <img src="img/Go back.png" height="50" width="50" onClick="back()"> </div>
 </div>
 
 <!--cover_photo_album-->
 <div style="display:none;" id="cover_photo_album">
-<div style="position:absolute; left:21%; top:100%;">
+<div style="position:absolute; left:21%; top:8%;">
 <img src="../../fb_users/<?php echo $v_gender; ?>/<?php echo $v_email; ?>/Cover/<?php echo $cover_img; ?>" style="height:200; width:200; box-shadow:0px 0px 5px 1px rgb(0,0,0);" onClick="open_cover_photo()">
 </div>
-<div style="position:absolute; left:18%; top:85%;"> <img src="img/Go back.png" height="50" width="50" onClick="back()"> </div>
+<div style="position:absolute; left:18%; top:8%;"> <img src="img/Go back.png" height="50" width="50" onClick="back()"> </div>
 </div>
 
 <!--timeline_photos_album bg-->
 <div style="display:none;" id="timeline_photos_album">
-<div style="position:absolute;left:15%; top:80%; height:1<?php echo $photos_count-2; ?>0%; width:70%; background:#FFF; box-shadow:0px -1px 5px 1px rgb(0,0,0); z-index:-1;">
+<div style="position:absolute;left:15%; top:8%; height:1<?php echo $photos_count-2; ?>0%; width:70%; background:#FFF; box-shadow:0px -1px 5px 1px rgb(0,0,0); z-index:-1;">
 </div>
 
 
@@ -132,15 +137,15 @@ function close_timeline_album_photo()
 }
 </script>
 
-<div style="position:absolute; left:18%; top:85%;">
+<div style="position:absolute; left:18%; top:8%;">
 <img src="img/Go back.png" height="50" width="50" onClick="back()">
 <table cellpadding="41">
 <tr>
 <?php
 	$tr=0;
-	$que_post_img=mysql_query("select * from user_post where user_id=$v_user_id and post_pic!=''and priority='Public' order by post_id desc");
+	$que_post_img=mysqli_query($conn,"select * from user_post where user_id=$v_user_id and post_pic!=''and priority='Public' order by post_id desc");
 	
-	while($post_img_data=mysql_fetch_array($que_post_img))
+	while($post_img_data=mysqli_fetch_array($que_post_img))
 	{
 		$tr=$tr+1;
 ?>
@@ -164,7 +169,7 @@ function close_timeline_album_photo()
 
 
 
-<div style="position:absolute;left:24%;top:220%; color:#E9EAED;">.</div>
+<div style="position:absolute;left:24%;top:9%; color:#E9EAED;">.</div>
 
 
 
